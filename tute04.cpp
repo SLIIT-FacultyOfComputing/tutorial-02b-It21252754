f19bca483b107cc6054458e6c2c6b30405fac68c
it21252754 <99489115+It21252754@users.noreply.github.com>
2022-03-04 17:41:50 +0000

    I have completed all exercises in tutorial 2.

--- tute04.cpp
+++ tute04.cpp
@@ -27,3 +27,17 @@ int main() {
   std::cout << std::endl;
   return 0;
 }
+long Factorial(int no)
+{
+  long fac=1;
+  int i;
+  for(i=no;i>=1;i--)
+    {
+      fac=fac*i;
+    }
+  return fac;
+}
+long nCr(int n, int r)
+{
+  return Factorial(n)/Factorial(r)*Factorial(n-r);
+}
